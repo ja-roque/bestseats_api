430864aa6e47fea9d2c115e18828a909043ec379
Javier <jarb95@gmail.com>
2020-04-26 02:10:37 -0600

    Create initialize for Venue object

--- bestseats/app/models/venue.rb
+++ bestseats/app/models/venue.rb
@@ -1,9 +1,13 @@
 class Venue
   attr_accessor :row_count, :column_count, :available_seats_hash, :data
+  ALPH = ("a".."z").to_a
 
   def initialize( venue_data )
     @row_count = venue_data.dig(:venue, :layout, :rows)
     @column_count = venue_data.dig(:venue, :layout, :columns)
+
+    char_to_num = ALPH.zip((0..@column_count*@row_count).step(@column_count).to_a).to_h
+
     @available_seats_hash = venue_data[:seats]
 
     venue = Array.new(@row_count)
@@ -15,6 +19,12 @@ class Venue
       end
 
     end
+
+    @all_seats = @data.flatten
+    @available_seats = @available_seats_hash.map { |id, data| @all_seats[char_to_num[data[:row]] + data[:column]-1] }
+    @best_seat = get_best_seat
   end
+
+  
 
 end
