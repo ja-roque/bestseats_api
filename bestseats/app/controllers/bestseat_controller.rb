0b8249904bb82d2c15440f5b86e2b98021995aa6
Javier <jarb95@gmail.com>
2020-04-26 22:03:36 -0600

    Return all seat data for FE rendering

--- bestseats/app/controllers/bestseat_controller.rb
+++ bestseats/app/controllers/bestseat_controller.rb
@@ -2,14 +2,14 @@ class BestseatController < ApplicationController
   before_action :permit_params
 
   def show
-    count = params[:count]
+    count = params[:count]&.to_i
     available_seats = params[:venue_data]
     # head :bad_request unless count && available_seats
 
     new_venue = Venue.new available_seats
-    best_seats = new_venue.find_best_available_seats count
-    puts best_seats
-    render json: best_seats
+    new_venue.find_best_available_seats count
+
+    render json: new_venue.all_seats
   end
 
   def permit_params
